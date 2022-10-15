64f9b8fb91e2c30201e05cc4cf01a6ff855445b3
telman03 <telmangadimov1@gmail.com>
2022-10-15 21:31:46 +0400

    Login service bug fixed

--- src/main/java/tinder/DAO/MessageDaoDB.java
+++ src/main/java/tinder/DAO/MessageDaoDB.java
@@ -7,7 +7,6 @@ import lombok.SneakyThrows;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 

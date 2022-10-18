46162f16ada573bf1b4a8af2bbe083e2119e6cfc
telman03 <telmangadimov1@gmail.com>
2022-10-18 23:29:11 +0400

    Procfile added

--- src/main/java/tinder/Database/DatabaseConnector.java
+++ src/main/java/tinder/Database/DatabaseConnector.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnector {
-    private static final String url = "jdbc:postgresql://localhost:5432/d2hmka3hjunhdn";
-    private static final String username = "ygioxnmirpouev";
-    private static final String password = "0f0ea151d3a0d12b6712395b40c1749092c2e83fae1eedcd14a4d812bae2ab49";
+    private static final String url = "jdbc:postgresql://localhost:5432/tinder";
+    private static final String username = "postgres";
+    private static final String password = "eden258";
 
     private static Connection connection;
 
